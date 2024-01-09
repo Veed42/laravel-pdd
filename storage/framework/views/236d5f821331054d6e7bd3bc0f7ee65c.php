ab58491efb0fe67614803e7b888c59cf181e10a8
Veed42 <valeraabramov96@gmail.com>
2024-01-09 22:26:01 +0300

    up

---
+++ storage/framework/views/236d5f821331054d6e7bd3bc0f7ee65c.php
@@ -0,0 +1,5 @@
+<footer class="py-2 navbar-brand text-center bg-black text-bg-danger">
+    <?php echo e(config('app.name', 'Laravel')); ?> <?php echo e(date(now()->format('Y'))); ?>
+
+</footer>
+<?php /**PATH C:\OSPanel\domains\pdd-laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
